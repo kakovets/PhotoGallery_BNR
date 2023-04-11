68d5b2e62a02d539f2bf3e7b4e84d822855c4f90
kakovets <victorentino@gmail.com>
2023-04-12 00:53:44 +0300

    Added repository, implemented Retrofit request and logged response.

--- app/src/main/java/com/kakovets/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/kakovets/photogallery/PhotoGalleryFragment.kt
@@ -1,6 +1,7 @@
 package com.kakovets.photogallery
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,9 +9,20 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
+private const val TAG = "PhotoGalleryFragment"
 class PhotoGalleryFragment: Fragment() {
 
     private lateinit var recyclerView: RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val flickrLiveData = FlickrFetchr().fetchContents()
+        flickrLiveData.observe(this) {response ->
+            Log.d(TAG, "Response received: $response")
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -19,8 +31,6 @@ class PhotoGalleryFragment: Fragment() {
         val view = inflater.inflate(R.layout.fragment_photo_gallery, container, false)
         recyclerView = view.findViewById(R.id.recyclerView)
         recyclerView.layoutManager = GridLayoutManager(context, 3)
-
-
         return view
     }
     companion object {
@@ -28,6 +38,4 @@ class PhotoGalleryFragment: Fragment() {
             return PhotoGalleryFragment()
         }
     }
-
-
 }
